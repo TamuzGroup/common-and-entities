366b7bd75bf7dcc23f76502326d563b24cd01203
Eden Elmalich <eelmalich2@gmail.com>
2021-10-25 18:22:24 +0300

    removed unnecessary logs

--- src/server/utils/helper.util.ts
+++ src/server/utils/helper.util.ts
@@ -2,11 +2,9 @@ import PDFDocument from "pdfkit";
 import fs from "fs";
 
 const createPdf = (text: string, fileName: string) => {
-  console.log({ text });
   // Create a document
   const doc = new PDFDocument();
 
-  console.log({ doc });
   doc.pipe(fs.createWriteStream(fileName));
   doc.text(text);
   doc.end();
