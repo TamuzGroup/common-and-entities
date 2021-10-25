7a7991b858a7375c6df0c72b44a0a676ec6c4d34
Alexander Mermelshtein <alexandermermelshtein@alexanders-mbp.com>
2021-10-25 16:57:35 +0300

    updating

--- src/config/mongodb/mongoConfig.ts
+++ src/config/mongodb/mongoConfig.ts
@@ -1,26 +1,37 @@
 const config = require("./config.json");
+//
+// namespace MongoDbConfig {
+//     export interface IMongoDBConnectionSettings {
+//         user: string;
+//         password: string;
+//         host: string;
+//         protocol: string;
+//         port: number;
+//         authentication: string;
+//         db: string;
+//     }
+// }
 
-namespace MongoDbConfig {
-    export interface IMongoDBConnectionSettings {
-        user: string;
-        password: string;
-        host: string;
-        protocol: string;
-        port: number;
-        authentication: string;
-        db: string;
-    }
+
+interface IMongoDBConnectionSettings {
+    user: string;
+    password: string;
+    host: string;
+    protocol: string;
+    port: number;
+    authentication: string;
+    db: string;
 }
 
 export default class MongoDbConfig {
-    static getMongoDBConnectionParams(env: string): MongoDbConfig.IMongoDBConnectionSettings {
+    static getMongoDBConnectionParams(env: string): IMongoDBConnectionSettings {
         switch (env) {
             case "dev":
-                return config.mongoLocalSettings as MongoDbConfig.IMongoDBConnectionSettings;
+                return config.mongoLocalSettings as IMongoDBConnectionSettings;
             case "prod":
-                return config.mongoSettings as MongoDbConfig.IMongoDBConnectionSettings;
+                return config.mongoSettings as IMongoDBConnectionSettings;
             default:
-                return config.mongoLocalSettings as MongoDbConfig.IMongoDBConnectionSettings;
+                return config.mongoLocalSettings as IMongoDBConnectionSettings;
         }
     }
 }
