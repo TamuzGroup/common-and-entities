a385e88e6ae4c7235b221aeea1060c53ff734db5
Matan Navon <matann@tamuzgroup.co.il>
2021-10-25 19:06:03 +0300

    test

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,10 @@ export class ApiError {
   public readonly statusCode: number;
 
   public readonly isOperational: boolean;
+  constructor(
+    statusCode: number,
+    message: string,
+    isOperational = true,
+    stack = ""
+  )
 };
