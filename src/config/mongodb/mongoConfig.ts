872e70a86cafe8cdee19b42b66d26a0c31a2edb2
Alexander Mermelshtein <alexandermermelshtein@alexanders-mbp.com>
2021-10-27 11:44:58 +0300

    saving befor merge with development

--- src/config/mongodb/mongoConfig.ts
+++ src/config/mongodb/mongoConfig.ts
@@ -1,42 +1,4 @@
 const config = require("./config.json");
-// //
-// // namespace MongoDbConfig {
-// //     export interface IMongoDBConnectionSettings {
-// //         user: string;
-// //         password: string;
-// //         host: string;
-// //         protocol: string;
-// //         port: number;
-// //         authentication: string;
-// //         db: string;
-// //     }
-// // }
-//
-//
-// interface IMongoDBConnectionSettings {
-//     user: string;
-//     password: string;
-//     host: string;
-//     protocol: string;
-//     port: number;
-//     authentication: string;
-//     db: string;
-// }
-//
-// export default class MongoDbConfig {
-//     static getMongoDBConnectionParams(env: string): IMongoDBConnectionSettings {
-//         switch (env) {
-//             case "dev":
-//                 return config.mongoLocalSettings as IMongoDBConnectionSettings;
-//             case "prod":
-//                 return config.mongoSettings as IMongoDBConnectionSettings;
-//             default:
-//                 return config.mongoLocalSettings as IMongoDBConnectionSettings;
-//         }
-//     }
-// }
-
-
 
 export interface IMongoDBConnectionSettings {
     user: string;
