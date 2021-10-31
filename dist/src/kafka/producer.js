843ecf21a6da9cff6c4ad99b0bef15ed6c863da3
Alexander Mermelshtein <alexandermermelshtein@alexanders-mbp.com>
2021-10-31 20:29:07 +0200

    updating

--- dist/src/kafka/producer.js
+++ dist/src/kafka/producer.js
@@ -1,12 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const kafkajs_1 = require("kafkajs");
+const logger_util_1 = __importDefault(require("../server/utils/logger.util"));
 class KafkaProducer {
     constructor(topic = "topic-test", clientId = "first-app", brokers = ["localhost:9092"]) {
         this.kafka = new kafkajs_1.Kafka({
             clientId,
             brokers,
         });
+        this.brokers = brokers;
         this.topic = topic;
         this.producer = this.kafka.producer();
     }
@@ -17,7 +22,9 @@ class KafkaProducer {
         });
     }
     connect() {
-        return this.producer.connect();
+        const connected = this.producer.connect();
+        logger_util_1.default.info("producer connected", "topic", this.topic, "brokers", this.brokers);
+        return connected;
     }
     disconnect() {
         return this.producer.disconnect();
