f54be55d1c5d0a30b420e959bad01a4478abb97c
Alexander Mermelshtein <alexandermermelshtein@alexanders-mbp.com>
2021-10-31 20:46:47 +0200

    updating

--- dist/src/kafka/kafkaConstants.js
+++ dist/src/kafka/kafkaConstants.js
@@ -4,15 +4,15 @@ const NOTIFICATION_SETTINGS = {
     GROUP_ID: "notifications_group",
     TOPIC: "notification_request",
     CLIENT_ID: "notifications_consumer_1",
-    BROKERS: ["localhost:9092"],
-    // BROKERS: ["kafka:9092"],
+    // BROKERS: ["localhost:9092"],
+    BROKERS: ["kafka:9092"],
 };
 const CLOUD_STORAGE_TOKEN_MNG = {
     GROUP_ID: "cloud_storage_token_mng_group_id",
     TOPIC: "cloud_storage_token_mng",
     CLIENT_ID: "cloud_storage_token_mng_consumer_id",
-    BROKERS: ["localhost:9092"],
-    // BROKERS: ["kafka:9092"],
+    // BROKERS: ["localhost:9092"],
+    BROKERS: ["kafka:9092"],
 };
 const kafkaConstants = {
     NOTIFICATION_SETTINGS,
